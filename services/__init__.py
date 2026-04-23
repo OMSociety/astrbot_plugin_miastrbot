6bb65a62548b8cab126788ddfb50187c31437f13
山海学社OMSociety <85862362+OMSociety@users.noreply.github.com>
2026-04-23 18:16:44 +0800

    refactor: 移除废弃的 xiaomi_service 导入

--- services/__init__.py
+++ services/__init__.py
@@ -3,16 +3,11 @@
 miastrbot 服务层
 """
 
-from .xiaomi_service import XiaomiService, XiaomiServiceError, XiaomiAuthError, XiaomiCommandError
 from .xiaomi_speaker_service import XiaomiSpeakerService, XiaomiSpeakerError, XiaomiSpeakerAuthError
 from .mihome_service import MiHomeService, MiHomeServiceError, MiHomeAuthError, MiHomeControlError
 from .tts_service import TTSServer, TTSServerError
 
 __all__ = [
-    "XiaomiService",
-    "XiaomiServiceError",
-    "XiaomiAuthError",
-    "XiaomiCommandError",
     "XiaomiSpeakerService",
     "XiaomiSpeakerError",
     "XiaomiSpeakerAuthError",
